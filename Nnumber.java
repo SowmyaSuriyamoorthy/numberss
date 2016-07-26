7931c16ae4e4709df48e4b52fcb62d6d0d95bbb1
SowmyaSuriyamoorthy <sowmyaselvi22@gmail.com>
2016-07-26 18:21:34 +0530

    Add files via upload

---
+++ Nnumber.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Nnumber {
+	public static void main (String[] args){
+int s = 0; 
+for (int i = 0; i <= 2; i++)
+		{
+		  s =s+ i; 
+		}
+ System.out.println(" sum= " + s);
+		}
+		}
+
